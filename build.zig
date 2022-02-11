729669ad7d86214d55dc2a0517bc79fee7021c3d
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-02-11 07:58:11 +0100

    Build using Zig

---
+++ build.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    const target = b.standardTargetOptions(.{});
+    const mode = b.standardReleaseOptions();
+
+    const udx = b.addStaticLibrary("udx", null);
+    udx.setTarget(target);
+    udx.setBuildMode(mode);
+    udx.linkLibC();
+    udx.linkSystemLibrary("libuv");
+    udx.addSystemIncludeDir("/usr/include");
+    udx.addLibPath("/usr/lib");
+    udx.addCSourceFiles(&.{
+        "src/cirbuf.c",
+        "src/fifo.c",
+        "src/udx.c",
+        "src/utils.c",
+    }, &.{
+        "-Wall",
+    });
+    udx.install();
+}
