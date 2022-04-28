57c0056ece271f3cd974bda6b313eb6418c72770
Mathias Buus <mathiasbuus@gmail.com>
2022-04-28 14:24:29 +0200

    use default timeouts

--- test/stream-parallel.js
+++ test/stream-parallel.js
@@ -3,7 +3,6 @@ const { makePairs, pipeStreamPairs } = require('./helpers')
 
 test('16 parallel streams on 1 socket', function (t) {
   const { streams, close } = makePairs(16, 'single')
-  t.timeout(1000 * 2)
   t.teardown(close)
   t.plan(1)
   const messageSize = 1024 * 64
@@ -15,7 +14,6 @@ test('16 parallel streams on 1 socket', function (t) {
 
 test('16 parallel streams on 16 sockets', function (t) {
   const { streams, close } = makePairs(16, 'multi')
-  t.timeout(1000 * 2)
   t.teardown(close)
   t.plan(1)
   const messageSize = 1024 * 64
