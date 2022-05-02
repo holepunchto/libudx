8b66b25e6c8b47aa1823b1127ae9e5fbedefb000
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-05-02 13:39:39 +0200

    Update C tests

--- test/stream-destroy.c
+++ test/stream-destroy.c
@@ -4,7 +4,8 @@
 #include "../include/udx.h"
 
 uv_loop_t loop;
-udx_t sock;
+udx_t udx;
+udx_socket_t sock;
 
 bool close_called = false;
 
@@ -12,7 +13,7 @@ void
 on_close (udx_stream_t *handle, int status) {
   assert(status == 0);
 
-  udx_close(&sock, NULL);
+  udx_socket_close(&sock, NULL);
 
   close_called = true;
 }
@@ -23,16 +24,19 @@ main () {
 
   uv_loop_init(&loop);
 
-  e = udx_init(&loop, &sock);
+  e = udx_init(&loop, &udx);
+  assert(e == 0);
+
+  e = udx_socket_init(&udx, &sock);
   assert(e == 0);
 
   struct sockaddr_in addr;
   uv_ip4_addr("127.0.0.1", 8081, &addr);
-  e = udx_bind(&sock, (struct sockaddr *) &addr);
+  e = udx_socket_bind(&sock, (struct sockaddr *) &addr);
   assert(e == 0);
 
   udx_stream_t stream;
-  e = udx_stream_init(&loop, &stream, 1);
+  e = udx_stream_init(&udx, &stream, 1);
   assert(e == 0);
 
   e = udx_stream_connect(&stream, &sock, 2, (struct sockaddr *) &addr, on_close);
