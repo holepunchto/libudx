3c1cf0e90d866a81316957b8120cf29b6c91463e
Lucas Barrena <lucasbarrena98@gmail.com>
2022-07-31 20:52:55 -0300

    add several tests for sockets

--- test/socket.js
+++ test/socket.js
@@ -1,5 +1,6 @@
 const test = require('brittle')
 const UDX = require('../')
+const UDXSocket = require('../lib/socket')
 
 test('can bind and close', async function (t) {
   const u = new UDX()
@@ -310,4 +311,143 @@ test('try send after close', async function (t) {
   a.close()
 
   t.is(a.trySend(Buffer.from('hello'), a.address().port), undefined)
+})
+
+test('connect to invalid host ip', async function (t) {
+  t.plan(1)
+
+  const u = new UDX()
+
+  const a = u.createSocket()
+  const s = u.createStream(1)
+
+  const invalidHost = '0.-1.0.0'
+
+  try {
+    s.connect(a, 2, 0, invalidHost)
+    t.fail('Should have given error')
+  } catch (error) {
+    t.is(error.message, `${invalidHost} is not a valid IP address`)
+  }
+})
+
+test('set TTL', async function (t) {
+  t.plan(2)
+
+  const u = new UDX()
+  const a = u.createSocket()
+
+  a.on('message', function (message) {
+    t.alike(message, Buffer.from('hello'))
+    a.close()
+  })
+
+  try {
+    a.setTTL(5)
+  } catch (error) {
+    t.is(error.message, 'Socket not active')
+  }
+
+  a.bind(0)
+  a.setTTL(5)
+
+  await a.send(Buffer.from('hello'), a.address().port)
+})
+
+test('get recv buffer size', async function (t) {
+  t.plan(2)
+
+  const u = new UDX()
+  const a = u.createSocket()
+
+  try {
+    a.getRecvBufferSize()
+  } catch (error) {
+    t.is(error.message, 'Socket not active')
+  }
+
+  a.bind(0)
+  t.ok(a.getRecvBufferSize() > 0)
+
+  await a.close()
+})
+
+test('set recv buffer size', async function (t) {
+  t.plan(2)
+
+  const u = new UDX()
+  const a = u.createSocket()
+
+  const NEW_BUFFER_SIZE = 8192
+
+  try {
+    a.setRecvBufferSize(NEW_BUFFER_SIZE)
+  } catch (error) {
+    t.is(error.message, 'Socket not active')
+  }
+
+  a.bind(0)
+  a.setRecvBufferSize(NEW_BUFFER_SIZE)
+
+  t.is(a.getRecvBufferSize(), NEW_BUFFER_SIZE * 2) // Why recv buffer is double?
+
+  await a.close()
+})
+
+test('get send buffer size', async function (t) {
+  t.plan(2)
+
+  const u = new UDX()
+  const a = u.createSocket()
+
+  try {
+    a.getSendBufferSize()
+  } catch (error) {
+    t.is(error.message, 'Socket not active')
+  }
+
+  a.bind(0)
+  t.ok(a.getSendBufferSize() > 0)
+
+  await a.close()
+})
+
+test('set send buffer size', async function (t) {
+  t.plan(2)
+
+  const u = new UDX()
+  const a = u.createSocket()
+
+  const NEW_BUFFER_SIZE = 8192
+
+  try {
+    a.setSendBufferSize(NEW_BUFFER_SIZE)
+  } catch (error) {
+    t.is(error.message, 'Socket not active')
+  }
+
+  a.bind(0)
+  a.setSendBufferSize(NEW_BUFFER_SIZE)
+
+  t.is(a.getSendBufferSize(), NEW_BUFFER_SIZE * 2) // Why send buffer is double?
+
+  await a.close()
+})
+
+test('UDXSocket - isIPv4', async function (t) {
+  t.is(UDXSocket.isIPv4('127.0.0.1'), true)
+  t.is(UDXSocket.isIPv4('::1'), false)
+  t.is(UDXSocket.isIPv4('0.-1.0.0'), false)
+})
+
+test('UDXSocket - isIPv6', async function (t) {
+  t.is(UDXSocket.isIPv6('127.0.0.1'), false)
+  t.is(UDXSocket.isIPv6('::1'), true)
+  t.is(UDXSocket.isIPv6('0.-1.0.0'), false)
+})
+
+test('UDXSocket - isIP', async function (t) {
+  t.is(UDXSocket.isIP('127.0.0.1'), 4)
+  t.is(UDXSocket.isIP('::1'), 6)
+  t.is(UDXSocket.isIP('0.-1.0.0'), 0)
 })
