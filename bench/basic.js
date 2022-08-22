fb2a8ada00216bbcec4bf77336c5d2da942444ef
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-08-22 14:48:47 +0200

    Add latency benchmark

--- bench/basic.js
+++ bench/basic.js
@@ -1,5 +1,6 @@
-const bench = require('nanobench-utils/nanobench')
-const { makePairs, pipeStreamPairs } = require('../helpers')
+const bench = require('nanobench')
+const byteSize = require('tiny-byte-size')
+const { makePairs, pipeStreamPairs } = require('../test/helpers')
 
 const STREAM_COUNTS = [1, 2, 4, 8]
 const MESSAGE_SIZES = [1024 * 64, 1024]
@@ -22,10 +23,10 @@ function benchmarkThroughput (b, streamCount, multiplexMode, messageSize, total)
   const { streams, close } = makePairs(streamCount, multiplexMode)
   const limit = total / streamCount
 
-  b.throughput(total)
   b.start()
   pipeStreamPairs(streams, messageSize, limit)
     .then(() => {
+      b.log(byteSize(total / b.elapsed() * 1e3) + '/s')
       b.end()
       close()
     })
