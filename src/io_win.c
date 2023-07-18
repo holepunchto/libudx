08eb7fdc910e592eacb6d6e7ec7fd2b51126f526
James Thomas <jthomas.dev@protonmail.com>
2023-07-18 10:48:25 -0400

    windows get local mtu

--- src/io_win.c
+++ src/io_win.c
@@ -6,8 +6,31 @@
 #include "io.h"
 
 int
-udx__get_link_mtu (const struct sockaddr *remote) {
-  return -1; // todo
+udx__get_link_mtu (const struct sockaddr *addr) {
+  assert(addr->sa_family == AF_INET || addr->sa_family == AF_INET6);
+
+  int s = socket(addr->sa_family, SOCK_DGRAM, 0);
+  if (s == -1) {
+    return -1;
+  }
+
+  int rc = connect(s, addr, addr->sa_family == AF_INET ? sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6));
+
+  if (rc == -1) {
+    return -1;
+  }
+
+  int mtu;
+  int mtu_opt_size = sizeof mtu;
+
+  rc = getsockopt(s, IPPROTO_IP, IP_MTU, (char *) &mtu, &mtu_opt_size);
+  if (rc == -1) {
+    close(s);
+    return -1;
+  }
+
+  closesocket(s);
+  return mtu;
 }
 
 ssize_t
