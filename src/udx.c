9a2827c1a3dc89a26571f659a9c40b4f97b65468
jthomas43 <129407891+jthomas43@users.noreply.github.com>
2023-09-26 03:11:01 -0400

    Fix/unprobeify (#154)
    * copied perf test from sendmmsg branch
    
    * trailing whitespace
    
    * padding field inadvertently set when retransmitting a failed mtu probe

--- src/udx.c
+++ src/udx.c
@@ -528,6 +528,7 @@ mtu_probeify_packet (udx_packet_t *pkt, int wanted_size) {
 static void
 mtu_unprobeify_packet (udx_packet_t *pkt) {
   assert(pkt->bufs_len == 3);
+  pkt->header[3] = 0;
   pkt->bufs[1] = pkt->bufs[2];
   pkt->bufs_len = 2;
 }
