e00e413ac01a8cba53aebe2b2be51eb8fa6c4d4d
James Thomas <jthomas.dev@protonmail.com>
2026-04-08 12:19:42 -0400

    add sequence wrapping test for sack tree

--- test/sack.c
+++ test/sack.c
@@ -38,6 +38,9 @@ main () {
     udx_sack_tree_insert(&tree, block + i);
   }
 
+  int middle_sack_block_start = tree.root->start;
+  assert(middle_sack_block_start == 11);
+
   i = 0;
   // todo: sack_block_min private api?
   // for public, cache tree.min, or have a udx_sack_block_min(udx_tree_t *tree) API
@@ -60,6 +63,42 @@ main () {
 
     udx_sack_tree_remove(&tree, min);
   }
+
+  assert(tree.root == tree.sentinel);
+
+  // some assurance that the code works with wrapped sequence numbers
+  // the following code re-inserts the sack blocks, but shifts them over to be near UINT32_MAX so that
+  // roughly half of them are sequence wrapped, then we check that the tree is still in the correct order
+  // [4294967285:4294967286] [4294967290:4294967292] [4294967292:4294967293] [4294967295:0]
+  // [1:3] [3:4] [5:7] [10:11] [13:14] [15:38]
+
+  int offset = UINT32_MAX - middle_sack_block_start;
+  for (i = 0; i < 10; i++) {
+    block[i].start += offset;
+    block[i].end += offset;
+    udx_sack_tree_insert(&tree, block + i);
+  }
+
+  i = 0;
+
+  p = udx_sack_tree_min(&tree);
+
+  while (p != NULL) {
+    assert(p->start == block[i].start);
+    assert(p->end == block[i].end);
+    p = udx_sack_tree_next(&tree, p);
+    i++;
+  }
+
+  for (i = 0; i < 10; i++) {
+    udx_sack_block_t *min = udx_sack_tree_min(&tree);
+
+    assert(min != NULL);
+    assert(min->start == block[i].start && min->end == block[i].end);
+
+    udx_sack_tree_remove(&tree, min);
+  }
+  assert(tree.root == tree.sentinel);
 
   return 0;
 }
