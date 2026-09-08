2826b26eb1a7e9ba6aef9079eff0003436bc0ee4
Marcus Pousette <marcus.pousette@tether.io>
2026-09-08 15:49:31 +0200

    fix: lower measured RTO minimum to 200ms

--- src/udx.c
+++ src/udx.c
@@ -1336,8 +1336,8 @@ ack_packet (udx_stream_t *stream, uint32_t seq, int sack, udx_rate_sample_t *rs)
 
     stream->tlp_permitted = true;
 
-    // RTO <- SRTT + max (G, K*RTTVAR) where K is 4 maxed with 1s
-    stream->rto = max_uint32(stream->srtt + 4 * stream->rttvar, 1000);
+    // RTO <- SRTT + max (G, K*RTTVAR) where K is 4, with a 200ms minimum.
+    stream->rto = max_uint32(stream->srtt + 4 * stream->rttvar, 200);
 
     if (stream->rto > UDX_RTO_MAX_MS) {
       debug_printf("rto: computed rto=%u ms, capping to %u ms\n", stream->rto, UDX_RTO_MAX_MS);
